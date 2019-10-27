8b33004d33395bf53df0d094715334ce4c2f6d1c
acq571 <mariaagustina.bonessoimmerso@haw-hamburg.de>
2019-10-27 21:32:47 +0100

    a.14

--- src/a14/BasetypeReader.java
+++ src/a14/BasetypeReader.java
@@ -1,5 +1,6 @@
 package a14;
 
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class BasetypeReader {
@@ -11,12 +12,21 @@ public class BasetypeReader {
     private static void countBaseTypes() {
         System.out.println("Aufgabe 1.4: Es werden Ihre Eingaben gelesen und ausgewertet");
         Scanner scanner = new Scanner(System.in);
-        int i = scanner.nextInt();
-        double d = scanner.nextDouble();
-        float f = scanner.nextFloat();
-        long l = scanner.nextLong();
-        boolean b = scanner.hasNextBoolean();
+        scanner.useDelimiter("");
+        Hashtable<Object, Integer> hashtable = new Hashtable<Object, Integer>();
+        hashtable.put((int) 4, 0);
 
-        System.out.println(i);
+        while (scanner.hasNext()) {
+            hashtable.put(scanner.nextInt(), 0);
+             String c = scanner.next();
+            if (c.charAt(0) == '\n' || c.charAt(0) == '\r') {
+                System.out.println("_");
+            } else {
+
+                hashtable.forEach(
+                        (k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+            }
+        }
     }
 }
+
