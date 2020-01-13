0b16aa6e0e29042c215c33e585101a9c3c34c798
ezk0 <sahin.tekes@haw-hamburg.de>
2020-01-13 12:41:17 +0100

    db angepasst

--- DB_Aufgabenblatt7/src/Ehemalige.java
+++ DB_Aufgabenblatt7/src/Ehemalige.java
@@ -6,7 +6,6 @@ import java.sql.*;
 //import java.sql.ResultSetMetaData;
 
 
-
 public class Ehemalige {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -15,11 +14,12 @@ public class Ehemalige {
         //Verbindungen aufzubauen
         Connection con;
         String url = "jdbc:oracle:thin:@ora14:1521:inf14";
-        con = DriverManager.getConnection(url, "acq571", "Fernanda13425023");   // statt "scott" die a-Kennung hinschreiben, statt "tiger" eigenes HAW-Passwort
+        con = DriverManager.getConnection(url, "acc387 ", "st8515449SS");   // statt "scott" die a-Kennung hinschreiben, statt "tiger" eigenes HAW-Passwort
 
         //Erstelle die Anweisung und übergebe an das DBMS
         Statement statement = con.createStatement();
-        String befehl = "INSERT INTO Ehemalige (EhemaligenID, Name, Vorname, Geburtsdatum, Geschlecht, Email, Telefonnummer) VALUES (17, 'Sanchez', 'Pedro', '03-06-93', 'männlich', 'pedro.sanchez@java.de', 040768932)";
+        String befehl = "insert into Ehemalige(Rufname, Vorname, Nachname, Geburtsname, Geburtsdatum, adresse, EMail, TelNr) \n" +
+                "VALUES ('Pepe', 'Pedro', 'Rosales', 'Pedro Sanchez', '03-06-93',adresse('Bachstrasse 111', 22083, 'Hamburg', 'Deutschland'), 'pedro.sanchez@java.de', 040768932)";
         int anzahl = statement.executeUpdate(befehl);
         statement.close();
 
@@ -27,25 +27,25 @@ public class Ehemalige {
         Statement statement2 = con.createStatement();
         String anfrage = "SELECT * FROM Ehemalige";
         ResultSet ergebnis = statement2.executeQuery(anfrage); //das Ergebnis aus der Konsole
-
+        String titel = "";
         //verarbeitet das Ergebnis
         while (ergebnis.next()) {
-            String titel = ergebnis.getString(1);
-            System.out.println("Ehemalige: " + titel);                // ergebnis verarbeitet ausgeben
+            titel = ergebnis.getString(1);
         }
+        System.out.println("Ehemalige: " + titel);                // ergebnis verarbeitet ausgeben
         ergebnis.close(); //result
         statement2.close(); //das Schließen muss nicht vergessen werden!
 
 
         Statement statement3 = con.createStatement();
-        String secondAnfrage = "select * from EhemaligeHochs,ehemalige\n" +
-                "where EhemaligeHochs.ehemaligeID = ehemalige.ehemaligenID \n" +
-                "and ehemaligehochs.bezeichnunghochs = 'HAW'";
+        String secondAnfrage = "SELECT * FROM Ehemalige e join hatbelegtstudiengang h on e.studentid = h.studentid join bietet_studiengang b on b.studiengangkuerzel = h.studiengangkuerzel where h.studiengangkuerzel = 'INF' and b.institutid = (select institutid from institution i where i.bezeichnung = 'HAW')";
         ResultSet zweitesErgebnis = statement3.executeQuery(secondAnfrage);
-
-        while (zweitesErgebnis.next()) {
-            String titel2 = zweitesErgebnis.getString(1);
+        String titel2 = "";
+        while (zweitesErgebnis.) {
+            titel2 = zweitesErgebnis.getString(1);
+            titel2 = titel2 + " " + zweitesErgebnis.getString(3);
             System.out.println("Ergebnis: " + titel2);
+
         }
         zweitesErgebnis.close();
         statement3.close();
